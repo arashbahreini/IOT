741746cb8e33dc843070e03dd88df1eef005436d
Arash Bahreini <arash@omniaretail.com>
2019-11-10 20:42:38 +0100

    Error handling in db.py file

--- main/db.py
+++ main/db.py
@@ -1,30 +1,40 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-from logger import *
-
+from logger import save_error_log
 
 
 class Context:
     cred = {}
+
     def __init__(self):
-        cred = credentials.Certificate('me-arash-firebase-adminsdk.json')
-        firebase_admin.initialize_app(cred, {
-            'databaseURL': 'https://me-arash.firebaseio.com/'
-        })
-        print("inited")
-    def add(self,path, data):
+        try:
+            cred = credentials.Certificate('me-arash-firebase-adminsdk.json')
+            firebase_admin.initialize_app(cred, {
+                'databaseURL': 'https://me-arash.firebaseio.com/'})
+        except Exception as e:
+            save_error_log(e, "db.py", "__init__(self)")
+
+    def add(self, path, data):
         try:
             ref = db.reference(path)
             return ref.push(data).key
         except Exception as e:
-            print('Error in Context/add')
-            print(str(e))
-    def delete(self,path,key):
-        ref = db.reference(path)
-        ref.child(key).delete()
+            save_error_log(e, "db.py", "add(self, path, data)")
+
+    def delete(self, path, key):
+        try:
+            ref = db.reference(path)
+            ref.child(key).delete()
+        except Exception as e:
+            save_error_log(e, "db.py", "delete(self, path, key)")
+
     def update(self):
         pass
+
     def get(self, path):
-        ref = db.reference(path)
-        return ref.get()
+        try:
+            ref = db.reference(path)
+            return ref.get()
+        except Exception as e:
+            save_error_log(e, "db.py", "get(self, path)")
--- main/logger.py
+++ main/logger.py
@@ -8,19 +8,5 @@ def save_error_log(exception, file, method, message=None):
         "file": file,
         "method": method
     }
-<<<<<<< HEAD
-    print(exception)
-    return 1#write_to_db("RPI-health", data)
-=======
-    return write_to_db("RPI-health", data)
-
->>>>>>> bd1e317a7ceeccf1269afe90ba622eea0e17a26c
-
-def write_to_db(db_name, data):
-    try:
-        from firebase import firebase
-        firebase = firebase.FirebaseApplication(
-            "https://me-arash.firebaseio.com/", None)
-        return firebase.post('/' + db_name + '/', data)
-    except Exception as e:
-        save_error_log(e, "Health/wrapper.py", "write_to_db")
+    print(str(data))
+    return 1  # write_to_db("RPI-health", data)
